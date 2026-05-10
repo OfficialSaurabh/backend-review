172ebbc5eb15be5031e7c6f16894ce9265d75cef
OfficialSaurabh <hmsaurabhkr@gmail.com>
2026-05-10 21:34:33 +0530

    remove modal listing that cause restrcition region

--- app/gemini_service.py
+++ app/gemini_service.py
@@ -2,8 +2,8 @@ import google.generativeai as genai
 from app.config import settings
 
 genai.configure(api_key=settings.GEMINI_API_KEY)
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
+# for m in genai.list_models():
+#     print(m.name, m.supported_generation_methods)
 
 model = genai.GenerativeModel("models/gemini-1.5-flash")
 
